f22698378f4b34103514009c1487565fd06cf4d8
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-10-27 11:29:10 -0500

    Login de usuario

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,15 +36,42 @@ const createUser = async (req = request, res = response) => {
     }
 };
 
-const loginUser = (req = request, res = response) => {
+const loginUser = async (req = request, res = response) => {
     const { email, password } = req.body;
 
-    res.json({
-        ok: true,
-        msg: 'login',
-        email,
-        password,
-    });
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Usuario no existe',
+            });
+        } else {
+            // confirmar los passwords
+            const validPassword = bcrypt.compareSync(password, user.password);
+
+            if (!validPassword) {
+                return res.status(400).json({
+                    ok: false,
+                    msg: 'Password incorrecto',
+                });
+            } else {
+                // generar JWT
+
+                return res.json({
+                    ok: true,
+                    uid: user.id,
+                    name: user.name,
+                });
+            }
+        }
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador',
+        });
+    }
 };
 
 const renewToken = (req = request, res = response) => {
