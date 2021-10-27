ad9c94edf7d81d91779587d829340c7750f4a050
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-10-26 22:11:43 -0500

    Recuperar información de un posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,16 +1,31 @@
 const { request, response } = require('express');
 
 const createUser = (req = request, res = response) => {
-    res.json({
+    const { name, email, password } = req.body;
+
+    if (name.length < 5) {
+        return res.status(400).json({
+            ok: false,
+            msg: 'El nombre debe de tener 5 letras',
+        });
+    }
+
+    return res.json({
         ok: true,
         msg: 'register',
+        name,
+        email,
+        password,
     });
 };
 
 const loginUser = (req = request, res = response) => {
+    const { email, password } = req.body;
     res.json({
         ok: true,
         msg: 'login',
+        email,
+        password,
     });
 };
 
